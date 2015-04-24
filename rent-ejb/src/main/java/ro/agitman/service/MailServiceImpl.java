fc6136ce5b468af1d865106d38d13cbd6a455359
Alexandru Gitman <edi.gitman@gmail.com>
2015-04-24 13:19:15 +0200

    Mail looks good.

--- rent-ejb/src/main/java/ro/agitman/service/MailServiceImpl.java
+++ rent-ejb/src/main/java/ro/agitman/service/MailServiceImpl.java
@@ -18,24 +18,40 @@ public class MailServiceImpl implements MailService {
 
     @Override
     public void sendConfirmMail(User u) {
-        mailSender.sendEmail(u.getEmail(), "Confirm your email", u.getRegToken());
+        mailSender.sendEmail(u.getEmail(), "Confirm your email", buildConfirm(u.getRegToken()));
     }
 
     @Override
     public void sendAdvertAdded(Advert advert) {
-        mailSender.sendEmail(advert.getUser().getEmail(), "Anunt adaugat", "Anunt adaugat " + advert.getId());
+        mailSender.sendEmail(advert.getUser().getEmail(), "Anunt adaugat", buildAdvertAdded(advert.getId()));
     }
 
     @Override
-    public void sendNewPwd(User user){
-        mailSender.sendEmail(user.getEmail(), "Schima parola", user.getRegToken());
+    public void sendNewPwd(User user) {
+        mailSender.sendEmail(user.getEmail(), "Schima parola", buildNewPwd(user.getRegToken()));
     }
 
-    private String buildConfirm(String token){
+    private String buildConfirm(String token) {
         StringBuilder sb = new StringBuilder("Bine ai venit La chirie \n\n");
         sb.append("\t Pentru a confirma inregistrarea acceseaza linkul: ");
-        sb.append("<a href='http://lachirie.ro/registerConfirm/" + token + "'>link</a>");
+        sb.append("<a href='http://lachirie.ro/registerConfirm/" + token + "'>link</a>.");
         sb.append("\n\nMultumim pentru inregistrare");
+        return sb.toString();
+    }
+
+    private String buildAdvertAdded(Long id) {
+        StringBuilder sb = new StringBuilder("Felicitari, \n\n");
+        sb.append("\t Anuntul tau a fost adaugat si este accesibil ");
+        sb.append("<a href='http://lachirie.ro/details/" + id + "'>aici</a>.");
+        sb.append("\n\nMultumim pentru ca folositi aplicatioa noastra");
+        return sb.toString();
+    }
+
+    private String buildNewPwd(String token) {
+        StringBuilder sb = new StringBuilder("Salut, \n\n");
+        sb.append("\t Recent ai facut o cerere pentru a-ti recupara parola, pentru a continua te rog acceseaza urmatorul link: \n\n");
+        sb.append("<a href='http://lachirie.ro/recoverConfirm/" + token + "'>http://lachirie.ro/recoverConfirm/" + token + "</a>.");
+        sb.append("\n\nMultumim pentru ca folosesti aplicatioa noastra");
         return sb.toString();
     }
 }
